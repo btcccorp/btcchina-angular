1aac34b598b903617cf39346fb94b9c870d35c57
Tommy Tang <1530234656@qq.com>
2015-03-30 11:56:22 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -90,4 +90,5 @@ module.exports = function (config) {
     if(process.env.TRAVIS){
         config.browsers = ['Chrome_travis_ci'];
     }
+    console.log('singleRun:' + Boolean(process.env.singleRun));
 };
